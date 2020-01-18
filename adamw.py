355f8623ef6ecf16185b6bc6b28e1ceeaf79fb88
Cebtenzzre <cebtenzzre@gmail.com>
2020-01-18 17:14:26 -0500

    Add __init__.py and fix deprecated Tensor.add usage

--- adamw.py
+++ adamw.py
@@ -28,7 +28,6 @@ class AdamW(Optimizer):
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
         defaults = dict(lr=lr, betas=betas, eps=eps,
                         weight_decay=weight_decay, amsgrad=amsgrad)
-        #super(AdamW, self).__init__(params, defaults)
         super().__init__(params, defaults)
 
     def step(self, closure=None):
@@ -72,8 +71,8 @@ class AdamW(Optimizer):
                 state['step'] += 1
 
                 # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
-                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
                 if amsgrad:
                     # Maintains the maximum of all 2nd moment running avg. till now
                     torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
@@ -86,6 +85,6 @@ class AdamW(Optimizer):
                 bias_correction2 = 1 - beta2 ** state['step']
                 step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
 
-                p.data.mul_(1 - group['weight_decay']).addcdiv_(-step_size, exp_avg, denom)
+                p.data.mul_(1 - group['weight_decay']).addcdiv_(exp_avg, denom, value=-step_size)
 
         return loss
